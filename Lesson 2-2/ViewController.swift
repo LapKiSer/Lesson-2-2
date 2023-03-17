e0ac9c7d4b6693c5642f3db9693c4984f8518d80
Lapkovskiy Kirill <lapkovskiykirill@gmail.com>
2023-03-17 18:16:55 +0300

    Add property and setup connection class

--- Lesson 2-2/ViewController.swift
+++ Lesson 2-2/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColorTrafficLight: UIView!
+    @IBOutlet var yellowColorTrafficLight: UIView!
+    @IBOutlet var greenColorTrafficLight: UIView!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redColorTrafficLight.layer.cornerRadius = 73
+        yellowColorTrafficLight.layer.cornerRadius = 73
+        greenColorTrafficLight.layer.cornerRadius = 73
+        switchButton.layer.cornerRadius = 15
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeLight() {
+        switchButton.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
